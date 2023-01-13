2ed5cc444833a834a954dfd6c56c93f140efa182
Stan <xiesirong.kaiseisougou@gmail.com>
2023-01-13 18:04:58 +0900

    ６ commit

--- src/main/java/com/syashiei/reggie/controller/CommonController.java
+++ src/main/java/com/syashiei/reggie/controller/CommonController.java
@@ -1,27 +1,89 @@
 package com.syashiei.reggie.controller;
 
+import com.syashiei.reggie.common.R;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import java.io.File;
 import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.UUID;
 
 @RestController
 @Slf4j
 @RequestMapping("/common")
 public class CommonController {
 
+    @Value("${reggie.path}")
+    private String basePath;
+
+    /**
+     * 文件上传
+     * @param file
+     * @return
+     */
     @PostMapping("/upload")
-    public void upload(MultipartFile file, HttpServletResponse response){
+    public R<String> upload(MultipartFile file){
+        //file是临时文件，需要存储到指定文件夹，不然会在请求结束后被删除
         log.info("获取上传文件");
 
-        //创建输入流获取文件
-        FileInputStream fileInputStream = new FileInputStream(file);
-        //把临时文件转存
+        //获取原始文件名和后缀名
+        String originalFileName = file.getOriginalFilename();
+        String suffix = originalFileName.substring(originalFileName.lastIndexOf("."));
 
-        //
+        //使用UUID创建新的文件名，防止重名造成文件覆盖
+        String fileName = UUID.randomUUID().toString() + suffix;
+
+        //创建一个目录对象
+        File dir = new File(basePath);
+        if (!dir.exists()){
+            dir.mkdirs();
+        }
+
+        try{
+            //将临时文件存到指定位置
+            file.transferTo(new File(basePath+fileName));
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return R.success(fileName);
     }
+
+    @GetMapping("/download")
+    public void download(String name, HttpServletResponse response){
+
+        /**
+         * 文件输出
+         */
+        try {
+            //创建输入流，通过输入流读取文件
+            FileInputStream fileInputStream = new FileInputStream(new File(basePath + name));
+
+            //创建输出流，像浏览器输出文件
+            response.setContentType("image/jpeg");//设置返回文件类型
+
+            ServletOutputStream outputStream = response.getOutputStream();
+
+            int len = 0;
+            byte[] bytes = new byte[1024];
+            while ((len = fileInputStream.read(bytes))!= -1){
+                outputStream.write(bytes,0,len);
+                outputStream.flush();
+            }
+
+            //关闭资源
+            outputStream.close();
+            fileInputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
